63ebef261efcf297c4b361211b1da110a37ea1ae
Branislav Usjak <91624413+branislav1989@users.noreply.github.com>
2025-07-24 00:57:05 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,19 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Datahosting cloud reseller Google cloud and Aws
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`DataHosting is company in Slovakia cloud reselling Google Cloud and Aws...total avalible hard drive space is 140tb with nearest datacenter located in Swiss Zurich
+Here is avalible aplications :
+Aws:S3,Cloud Tail,Athena,Tranfer Family(Ftp,Smtp), Backup,DynamoDb,Sns
+Google Cloud:Cloud Storage,Bigquery,Datastore,Pub/Sub,Pub/Sub Lite,BackupDR,Datacatalog,Cloud Funtions,DLP,Dataplex,Analytic Hub,NetApp Cloud,Dell Emc Cloud..
+If customer need some specific aplication but its not on list ...aplication must be compatible with Storage...if somebody interested im gona add you manualy..in comment im gona add commands in cloud shell to join project
+
+Contact email:branislavusjak1989@gmail.com
+Whatsapp-Viber:+421951044387
+https://medium.com/@branislavusjak1989/streamline-your-business-with-aws-solutions-s3-storage-cloudtrail-sns-backup-and-transfer-a3126ec22dba
+https://medium.com/@branislavusjak1989/datahosting-is-a-leading-company-specializing-in-google-cloud-database-applications-providing-f0950b277cba`}
       </p>
       <div className="my-8">
         <BlogPosts />
